78388ed0f9a3df8d686bce9b32a794414e1ecc15
gabydi106 <gabydi106@gmail.com>
2015-04-25 16:38:57 -0400

    --added main HTML and CSS files

---
+++ results.php
@@ -0,0 +1,204 @@
+<html>
+<body>
+
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>GSL - Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.html"><span class="glyphicon glyphicon-home"></span></a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="about.html">About</a>
+                    </li>
+                    <li><a href="services.html">Services</a>
+                    </li>
+                    <li><a href="contact.html">Contact</a>
+                    </li>
+                </ul>
+            <form class="navbar-form navbar-right" role="search" method="post" action="results.php">
+                <div class="form-group">
+                    <input type="text" name="query" class="form-control" placeholder="Search products...">
+                </div>
+                <button id="submitSearch" type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span>
+                </button>
+            </form>
+            </div>
+            <!-- /.navbar-collapse -->
+         
+
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-3">
+                <img src="logo.jpg">
+                 <h5>Serving Your Document Needs</h5>
+                 <br><br><br><br><br><br><br><br>
+                <div class="list-group">
+                    <span class="glyphicon glyphicon-phone-alt"></span>  +44 020 3174 8056 <br> <br>
+                    <span class="glyphicon glyphicon-print"></span>  +44 020 3174 8057 <br> <br>
+                    <span class="glyphicon glyphicon-envelope"></span>  info@gslphotocopiers.org.uk <br> <br>
+                    <span class="glyphicon glyphicon-lock"></span>
+                    Golden Crescent Industrial Estate, Golden Crescent, Hayes
+                    <br>  Middlesex, UB3 1AQ
+            </div>
+               
+            </div>
+
+            <div class="col-md-9">
+
+                <div class="row carousel-holder">
+
+                    <div class="col-md-12">
+                        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img class="slide-image" src="img/img1.jpg" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="img/img6.jpg" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="img/img7.jpg" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="img/img4.jpg" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="slide-image" src="img/img5.jpeg" alt="">
+                                </div>
+                            </div>
+                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+	                Viewing results for: <b>  <?php  if (isset($_POST)) { echo $_POST['query']; } ?> </b>	<br> 
+
+	                <?php
+
+	                	if (isset($_POST)) {
+							$q = $_POST['query'];
+
+							$con = mysqli_connect('localhost','root','password','gsl');
+							if (!$con) {
+							  die('Could not connect: ' . mysqli_error($con));
+							}
+
+							mysqli_select_db($con,"gsl");
+							$sql="SELECT * FROM products WHERE ProductName like '%$q' or ProductName like '$q%' or ProductName like '% $q %'";
+							$result = mysqli_query($con,$sql);
+
+							if (!$result) {
+							    printf("Error: %s\n", mysqli_error($con));
+							    exit();
+							}
+
+							echo "<table border='1' class='table table-striped'>
+							<tr>
+							<th>Manufacturer</th>
+							<th>Product Name</th>
+							<th>Picture</th>
+							<th>MSRP</th>
+							</tr>";
+
+							while($row = mysqli_fetch_array($result)) {
+							  echo "<tr>";
+							  echo "<td>" . $row['ProductBrand'] . "</td>";
+							  echo "<td>" . $row['ProductName'] . "</td>";
+							  echo "<td>" . $row['ProductImgPath'] . "</td>";
+							  echo "<td>" . $row['ProductPrice'] . "</td>";
+							  
+
+							  echo "</tr>";
+							}
+							echo "</table>";
+
+							mysqli_close($con);
+						}
+					?>
+
+           		</div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Glebe Services Ltd. 2015</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
